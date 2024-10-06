0605fdf01fca1dde44a9309930ae0fcc66ce24e5
shree081297 <srikanthkurella123@gmail.com>
2024-10-06 22:21:20 +0530

    Third commit

--- src/test/java/TestCases/BaseClass.java
+++ src/test/java/TestCases/BaseClass.java
@@ -8,6 +8,7 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.*;
 
 public class BaseClass {
@@ -18,7 +19,13 @@ public class BaseClass {
 
 	@BeforeMethod(alwaysRun=true)
 	public void setUpDriver() {
-		driver = new ChromeDriver();
+		String browser=System.getProperty("Browser");
+		if(browser.equalsIgnoreCase("firefox")) {
+			driver=new FirefoxDriver();
+		}else {
+			driver = new ChromeDriver();
+	
+		}
 		driver.manage().window().maximize();
 		driver.get("https://www.simplilearn.com/");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
